baea1ee22a1b95c4e506a367b58e634d3cae98cb
Shubham <shubhamsinha2050@gmail.com>
2019-09-24 20:27:48 +0530

    Added the models for the backend

--- movierater/api/admin.py
+++ movierater/api/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Movie, Rating
 
 # Register your models here.
+
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description')
+
+
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'movie', 'stars')
+
+
+admin.site.register(Movie, MovieAdmin)
+admin.site.register(Rating, RatingAdmin)
--- movierater/api/models.py
+++ movierater/api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=32)
+    description = models.CharField(max_length=360)
+
+
+class Rating(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+
+    class Meta:
+        unique_together = (('user', 'movie'), )
+        index_together = (('user', 'movie'), )
+
