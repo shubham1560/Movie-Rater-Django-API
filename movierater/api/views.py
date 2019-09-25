aeb77062397ed5c303a7740415dbcb6db873899f
Shubham <shubhamsinha2050@gmail.com>
2019-09-25 20:33:17 +0530

    Added the custom method

--- movierater/api/views.py
+++ movierater/api/views.py
@@ -1,9 +1,11 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from .models import Movie, Rating
 from .serializers import MovieSerializer, RatingSerializer, MovieFullSerializer
+from rest_framework.response import Response
 # Create your views here.
+from rest_framework.decorators import action
 
 
 def first(request):
@@ -22,6 +24,11 @@ class MovieViewSet(viewsets.ModelViewSet):
         if self.request.method in ['GET']:
             return MovieFullSerializer
         return MovieSerializer
+
+    @action(detail=True, methods=['POST'])
+    def rate_movie(self, request, pk=None):
+        response = {'message': "it's working"}
+        return Response(response, status=status.HTTP_200_OK)
 
 
 class RatingViewSet(viewsets.ModelViewSet):
