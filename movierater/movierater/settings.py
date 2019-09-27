f1f3f934879cb9acaddeb3ea91159f1b0a783b36
Shubham <shubhamsinha2050@gmail.com>
2019-09-27 23:32:11 +0530

    added user restriction, can't view data without login

--- movierater/movierater/settings.py
+++ movierater/movierater/settings.py
@@ -83,6 +83,12 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    )
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
